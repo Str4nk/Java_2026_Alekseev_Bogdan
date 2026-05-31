4aced0b67818c523c8cba272d77f88dc50d1516c
Str4nk <Milko11447700@mail.ru>
2026-05-31 16:49:42 +0300

    Update App.java

--- App.java
+++ App.java
@@ -1,55 +1,26 @@
+public class App {
+    public static void main(String[] args) {
+        Vehicle vehicle1 = new Vehicle();
+        Vehicle vehicle2 = new Car();
 
-    public class App {
-        public static void main(String[] args) {
-            Shape r = new Rectangle(5, 4);
-            Shape s = new Square(4);
-
-            System.out.println("Rectangle area: " + r.getArea()); // 20
-            System.out.println("Square area: " + s.getArea());     // 16
-
-            // Дополнительная демонстрация: одна и та же функция работает с любым Shape
-            printArea(new Rectangle(6, 3));
-            printArea(new Square(5));
-        }
-
-        static void printArea(Shape shape) {
-            System.out.println("Area = " + shape.getArea());
-        }
+        moveVehicle(vehicle1);
+        moveVehicle(vehicle2);
     }
 
-    interface Shape {
-        int getArea();
+    static void moveVehicle(Vehicle vehicle) {
+        vehicle.move();
     }
+}
 
-    class Rectangle implements Shape {
-        private final int width;
-        private final int height;
-
-        public Rectangle(int width, int height) {
-            this.width = width;
-            this.height = height;
-        }
-
-        @Override
-        public int getArea() {
-            return width * height;
-        }
-
-        public int getWidth() { return width; }
-        public int getHeight() { return height; }
+class Vehicle {
+    public void move() {
+        System.out.println("Транспорт движется");
     }
+}
 
-    class Square implements Shape {
-        private final int side;
-
-        public Square(int side) {
-            this.side = side;
-        }
-
-        @Override
-        public int getArea() {
-            return side * side;
-        }
-
-        public int getSide() { return side; }
+class Car extends Vehicle {
+    @Override
+    public void move() {
+        System.out.println("Машина едет");
     }
+}
