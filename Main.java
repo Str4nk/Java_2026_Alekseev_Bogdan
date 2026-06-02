194a0448e337f0469947188df191e93f25b06c4a
Str4nk <Milko11447700@mail.ru>
2026-06-02 16:40:26 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,43 @@
-package package1;
-
 public class Main {
+    
+    // Статическая переменная (принадлежит классу)
+    public static int a = 1;
+    public static int b = 2;
+    
+    // Статический метод
+    public static void printVars() {
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+    
     public static void main(String[] args) {
-        System.out.println("=== ПРОВЕРКА ДОСТУПА ИЗ package1 ===");
         
-        // Тест из родительского класса
-        Parent p = new Parent();
-        p.showPrivateAccess();
+        System.out.println("=== ВАРИАНТЫ ВЫЗОВА printVars() ===\n");
         
-        // Тест из класса в том же пакете
-        SamePackage sp = new SamePackage();
-        sp.testAccess();
+        // Способ 1: Через имя класса (рекомендуемый)
+        System.out.println("1. Через имя класса:");
+        Main.printVars();
+        
+        // Способ 2: Через объект класса
+        System.out.println("\n2. Через объект класса:");
+        Main obj = new Main();
+        obj.printVars();
+        
+        // Способ 3: Прямой вызов (из статического метода того же класса)
+        System.out.println("\n3. Прямой вызов:");
+        printVars();
+        
+        // Способ 4: Через null-ссылку (работает!)
+        System.out.println("\n4. Через null-ссылку:");
+        Main nullObj = null;
+        nullObj.printVars();
+        
+        // Дополнительно: вызов из другого статического метода
+        System.out.println("\n5. Вызов из другого метода:");
+        callPrintVars();
+    }
+    
+    public static void callPrintVars() {
+        printVars();
     }
 }
